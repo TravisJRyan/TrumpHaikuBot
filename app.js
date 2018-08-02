42664ebfa46d0be39a4c47b359819a566ea29042
Travis Ryan <traryan@proofpoint.com>
2018-08-02 16:09:44 -0400

    simple dashboard mockup

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const mysql = require("mysql");
 const express = require('express');
+const bodyParser = require('body-parser');
 const secretVars = JSON.parse(fs.readFileSync('secret.json', 'utf8'));
 
 // Connection Object for MySQL
@@ -11,14 +12,14 @@ const db = mysql.createConnection({
     database: secretVars[0]["dbDatabase"]
 });
 
-let selectSql = "SELECT * FROM Haiku";
+/*let selectSql = "SELECT * FROM Haiku";
 let selectQuery = db.query(selectSql, (err, results) => {
     if (err) {
         console.log(err);
     } else {
         console.log(results);
     }
-});
+});*/
 
 //Express Setup
 const app = express();
@@ -27,3 +28,23 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+    let selectSql = "SELECT * FROM Haiku";
+    let selectQuery = db.query(selectSql, (err, results) => {
+        if (err) {
+            res.send(err);
+        } else {
+            console.log(results);
+            res.render('dashboard', {
+                haikus: results
+            });
+        }
+    });
+});
+
+// Start server listening
+app.listen('3000', () => {
+    console.log('Server started on port 3000');
+});
